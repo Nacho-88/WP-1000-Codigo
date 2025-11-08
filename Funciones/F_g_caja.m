7dd9a3a3a9f29e467759e4459345bc22efd49ae2
francisco-hernandezr <francisco.hernandezr@edu.uah.es>
2025-11-08 13:48:27 +0100

    Update F_g_caja.m

--- Funciones/F_g_caja.m
+++ Funciones/F_g_caja.m
@@ -1,4 +1,3 @@
-
 % F_g_caja.m
 % ---------------------------------------------------------------
 % Cálculo de la fuerza gravitatoria del payload (caja)
@@ -9,21 +8,36 @@
 % Ámbito: válido tanto en ascenso como en descenso
 %
 % Inputs:
-%   z      -> altitud real [m]
-%   dz_dt  -> velocidad vertical [m/s] (no se usa, pero se mantiene porque en un futuro se podrá diferenciar entre acenso y descenso)
-%
+%   z         -> altitud real [m]
+%   dz_dt     -> velocidad vertical [m/s] (actualmente no se usa)
+%   isFalling -> booleano (true si está descendiendo, false si ascendiendo)
 %
 % Output:
-%   F      -> fuerza gravitatoria [N]
+%   F         -> fuerza gravitatoria [N]
 %
-% Dependencias (definidas en el main estas ctes y variables):
+% Dependencias (definidas en el main):
 %   G, M_T, R_T, m_caja, l_c, L_z
 %   r_globo(z)
 % ================================================================
 
-function F = F_g_caja(z, dz_dt) 
-    F = (G * M_T * m_caja) ./ ( (R_T + z - r_globo(z) + l_c - L_z/2).^2 );
+function F = F_g_caja(z, dz_dt, isFalling) 
+    
+    % Aceleración gravitatoria efectiva
+    g_eff = (G * M_T) ./ ( (R_T + z - r_globo(z) + l_c - L_z/2).^2 );
+    
+    % Distinción entre ascenso y descenso con el nuevo parámetro
+    if isFalling
+        %DESCENSO
+       
+        F = m_caja * g_eff;
+    else
+        %ASCENSO
+       
+        F = m_caja * g_eff;
+    end
+
 end
+
 
 
 
